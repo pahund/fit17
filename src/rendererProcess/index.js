2cabb8be5299d533a65f4e268e9348880a03effc
Hund <pahund@lm-ber-14501643.speedport_w_723v_1_43_000>
2017-04-29 19:37:37 +0200

    added highcharts lib
    Change-Id: I00b9b48263851b25620daa246788322280dbf4a0

--- src/rendererProcess/index.js
+++ src/rendererProcess/index.js
@@ -1,5 +1,30 @@
+const Highcharts = require('highcharts');
 const ipcRenderer = require('electron').ipcRenderer;
 ipcRenderer.on('data', function (event, data) {
     console.log(data);
+});
+
+Highcharts.chart('container', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Fruit Consumption'
+    },
+    xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+        title: {
+            text: 'Fruit eaten'
+        }
+    },
+    series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+    }, {
+        name: 'John',
+        data: [5, 7, 3]
+    }]
 });
 
